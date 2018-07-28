e8bc25ff55fa1726296c3c6073582bee26d1e52b
wangzhizhou <824219521@qq.com>
2018-07-28 22:08:19 +0800

    download library complete

--- GameDownloader.py
+++ GameDownloader.py
@@ -3,8 +3,12 @@ from Config import Config
 import json
 import requests
 import os
+import sys
+import platform
+import uuid
 
 class GameDownloader:
+
 
     def __init__(self, version):
         self._game=None
@@ -41,9 +45,22 @@ class GameDownloader:
             self._assets = self.loadJSON(index_json_path)
         return self._assets
 
+    def getClassPathList(self):
+        pass
+
+    def platformType(self):
+        '''OS type'''
+        system = {
+            'Linux':'linux',
+            'Darwin':'osx',
+            'Windows': 'windows'
+        }
+        return system[platform.system()]
+
 # Assets
 
     def downloadAssetIndex(self):
+        '''Download Game Asset Index JSON file'''
         index_json_url = self.game().get('assetIndex').get('url')
         index_json_str = requests.get(index_json_url).text
         index_json_path= os.path.join(self.config.assets_indexes_dir(), os.path.basename(index_json_url))
@@ -51,6 +68,7 @@ class GameDownloader:
             f.write(index_json_str)
 
     def downloadAssetObjects(self):
+        '''Download Game Asset Objects'''
         objects = self.assets().get('objects')
         total = len(objects)
         index = 0
@@ -68,8 +86,45 @@ class GameDownloader:
             except:
                 print(outInfo + "FAILED!")
                 continue
-            
 
+# Library
+
+    def donwloadLibraries(self):
+        ''' download libraries'''
+        libs = self.game().get('libraries')
+        total = len(libs)
+        index = 0
+        for lib in libs: 
+            libName = lib.get('name')
+            downloads = lib.get('downloads')
+
+            rules = lib.get('rules')
+            if None != rules:
+                for rule in rules:
+                    if None != rule:
+                        if rule.get('action') == 'disallow':
+                            if rule.get('os').get('name') == self.platformType():
+                                print(libName + 'is disallowed')
+                                continue
+
+            libPath = None
+            url = None
+            if 'natives' in lib:
+                platform = lib.get('natives').get(self.platformType())
+                if platform == None:
+                    print('Error: no platform jar - ' +  libName)
+                    continue
+                else:
+                    libPath = downloads.get('classifiers').get(platform).get('path')
+                    url = downloads.get('classifiers').get(platform).get('url')
+            else:
+                libPath = downloads.get('artifact').get('path')
+                url = downloads.get('artifact').get('url')
+
+            fileDir = self.config.client_library_dir(libPath)
+            self.download(url,fileDir)
+            index = index + 1
+            print('%s(%d/%d)' % (os.path.basename(url), index, total))
 # Client
 
     def downloadClient(self):
@@ -79,9 +134,7 @@ class GameDownloader:
         self.download(clientUrl,self.config.client_jar_path())
         print("Client Download Completed!")
 
-
 # Server
-
     def downloadServer(self):
         '''Download Server Jar File'''
         serverUrl = self.game().get('downloads').get('server').get('url')
@@ -89,11 +142,12 @@ class GameDownloader:
         self.download(serverUrl,self.config.server_jar_path())
         print("Server Download Completed!")
 
-
 if __name__ == '__main__':
     game = GameDownloader('1.13')
     # game.downloadGameJSON()
     # game.downloadClient()
     # game.downloadServer()
     # game.downloadAssetIndex()
-    game.downloadAssetObjects()
+    # game.downloadAssetObjects()
+    game.donwloadLibraries()
+    
