d5e02cc2afce1aba34b914f55eb6406c5f193650
wangzhizhou <824219521@qq.com>
2018-07-29 08:33:31 +0800

    completed!

---
+++ OrzMC/__init__.py
@@ -0,0 +1,1 @@
+from .OrzMC import startClient
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+    name="OrzMC",
+    version="1.0",
+    description="A project for deployment of minecraft game.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="王志舟<wangzhizhou>",
+    author_email="824219521@qq.com",
+    maintainer='王志舟',
+    maintainer_email='824219521@qq.com',
+    url="https://github.com/OrzGeeker/OrzMC.git",
+    packages=setuptools.find_packages(),
+    platforms = ['all'],
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'requests',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'orzmc = OrzMC:startClient'
+        ]
+    }
+)
