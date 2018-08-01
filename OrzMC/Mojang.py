b2bbdfba86734cf4e8f842f9d69aea662b20d2b5
wangzhizhou <824219521@qq.com>
2018-08-02 02:58:55 +0800

    add cache function

--- OrzMC/Mojang.py
+++ OrzMC/Mojang.py
@@ -8,24 +8,29 @@ class Mojang:
     version_list_url = 'https://launchermeta.mojang.com/mc/game/version_manifest.json'
     asset_base_url = 'https://resources.download.minecraft.net/'
 
+    versions = None
+
     @classmethod
     def get_version_list(cls):
         '''Get All Version Game Configuration And Cache it if need'''
-        cacheFilePath = os.path.join(Config.GAME_ROOT_DIR,os.path.basename(Mojang.version_list_url))
-        resp = None
-        if os.path.exists(cacheFilePath):
-            with open(cacheFilePath,'r') as cache:
-                resp = json.load(cache)
-                print('Use Cache File For Game Version Manifest JSON File')
-        else:
-            resp = json.loads(requests.get(Mojang.version_list_url).text)
-            with open(cacheFilePath,'w') as cacheFile:
-                json.dump(resp,cacheFile)
-                print('Download Game Version Manifest JSON File from Mojang server and cached')
+        if None == Mojang.versions:
+            cacheFilePath = os.path.join(Config.GAME_ROOT_DIR,os.path.basename(Mojang.version_list_url))
+            resp = None
+            if os.path.exists(cacheFilePath):
+                with open(cacheFilePath,'r') as cache:
+                    resp = json.load(cache)
+                    print('Use Cache File For Game Version Manifest JSON File')
+            else:
+                resp = json.loads(requests.get(Mojang.version_list_url).text)
+                with open(cacheFilePath,'w') as cacheFile:
+                    json.dump(resp,cacheFile)
+                    print('Download Game Version Manifest JSON File from Mojang server and cached')
 
-        versions = resp.get('versions')
-        
-        return versions
+            Mojang.versions = resp.get('versions')
+        else:
+            print('use cached info in memory')
+            
+        return Mojang.versions
 
     @classmethod
     def get_release_version_list(cls):
@@ -39,8 +44,8 @@ class Mojang:
         releases = list(filter(lambda release: release.get('id') == id, Mojang.get_release_version_list()))
         if len(releases) > 0 : 
             url = releases[0].get('url')
-            jsonStr = requests.get(url).text
-            return jsonStr
+            hash = os.path.split(os.path.dirname(url))[-1]
+            return (url, hash)
         else:
             return None
         
