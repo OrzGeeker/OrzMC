0afa46c4696028e64c22178b97a6b483025e9cf5
wangzhizhou <824219521@qq.com>
2018-09-23 15:41:56 +0800

    increase version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="OrzMC",
-    version="1.0.4",
+    version="1.0.5",
     description="A project for deployment of minecraft game.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
@@ -26,6 +26,7 @@ setuptools.setup(
     ),
     install_requires=[
         'requests',
+        'twine'
     ],
     entry_points = {
         'console_scripts': [
