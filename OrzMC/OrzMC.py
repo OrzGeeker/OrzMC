70b89470036cc0d1fc27d5dbe932b7e7dc7a98d7
wangzhizhou <824219521@qq.com>
2018-09-23 15:37:43 +0800

    add the version and username support

--- OrzMC/OrzMC.py
+++ OrzMC/OrzMC.py
@@ -1,11 +1,31 @@
 from .GameDownloader import GameDownloader
+import sys, getopt
 
 def startClient():
-    game = GameDownloader('1.13')
-    game.downloadGameJSON()
-    game.downloadClient()
-    game.downloadServer()
-    game.downloadAssetIndex()
-    game.downloadAssetObjects()
-    game.donwloadLibraries()
-    game.startCient(user='guest')
+
+    version = "1.13"
+    username = "guest"
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "v:u:", ["version=", "username="])
+        for o, a in opts:
+            if o in ["-v", "--version"]:
+                if len(a) > 0:
+                    version = a
+            if o in ["-u", "--username"]:
+                if len(a) > 0:
+                    username = a
+
+        game = GameDownloader(version)
+        game.downloadGameJSON()
+        game.downloadClient()
+        game.downloadServer()
+        game.downloadAssetIndex()
+        game.downloadAssetObjects()
+        game.donwloadLibraries()
+        game.startCient(user=username)
+
+    except getopt.GetoptError:
+        print("The arguments is invalid!")
+    
+
