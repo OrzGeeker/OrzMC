36c0be77212e36a66275a27164a87463f77f87cc
wangzhizhou <824219521@qq.com>
2018-11-11 14:21:40 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="OrzMC",
-    version="1.0.5",
+    version="1.0.6",
     description="A project for deployment of minecraft game.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
@@ -14,16 +14,18 @@ setuptools.setup(
     url="https://github.com/OrzGeeker/OrzMC.git",
     packages=setuptools.find_packages(),
     platforms = ['all'],
+    keywords='minecraft python',
     classifiers=(
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: MacOS",
+        "License :: OSI Approved :: Apache Software License"
     ),
+    python_requires='>=2.7, <4',
     install_requires=[
         'requests',
         'twine'
