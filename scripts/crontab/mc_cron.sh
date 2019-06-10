fa7e0667ce233c82a7eac6fa97a42ca072b0caa2
wangzhizhou <824219521@qq.com>
2019-06-11 03:40:57 +0800

    add crontab script

--- scripts/crontab/mc_cron.sh
+++ scripts/crontab/mc_cron.sh
@@ -3,7 +3,9 @@
 TITLE="jokermc"
 
 function exec() {
-	/usr/bin/screen -p 0 -S $TITLE -X eval 'stuff "title @a actionbar {\\"text\\":\\"请各位冒险家们合理安排游戏时间， 注意早点休息哦～～～\\",\\"color\\":\\"yellow\\",\\"bold\\":\\"true\\"}"\\015'
+	/usr/bin/screen -p 0 -S $TITLE -X eval 'stuff "title @a times 10 100 10"\\015'
+	/usr/bin/screen -p 0 -S $TITLE -X eval 'stuff "title @a title {\\"text\\":\\"温馨提示\\",\\"color\\":\\"white\\",\\"bold\\":\\"true\\"}"\\015'
+	/usr/bin/screen -p 0 -S $TITLE -X eval 'stuff "title @a subtitle {\\"text\\":\\"各位冒险家们注意早点休息啦!\\",\\"color\\":\\"yellow\\",\\"bold\\":\\"true\\"}"\\015'
 }
 
 exec
