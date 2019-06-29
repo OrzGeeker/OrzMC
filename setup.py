7a8e72e26c06df0f3cabea948c2eafb1f50cb432
wangzhizhou <824219521@qq.com>
2019-06-30 02:41:46 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="OrzMC",
-    version="1.0.30",
+    version="1.0.31",
     description="A project for deployment of minecraft game.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
