7c67d2f698494470daa0ab6e7b1d15719c5d9f68
wangzhizhou <824219521@qq.com>
2019-08-05 01:16:13 +0800

    package on mac successfully

--- OrzMCL/OrzMCL.py
+++ OrzMCL/OrzMCL.py
@@ -1,10 +1,20 @@
 
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.graphics import Color, Rectangle
 
 class OrzMCL(App):
     def build(self):
-        return Button(text='Hello World')
+        layout = BoxLayout(padding=10)
+        button = Button(text='Hello World', size_hint = (0.5, 0.5))
+        layout.add_widget(button)
+        return layout
 
 
 def run():
