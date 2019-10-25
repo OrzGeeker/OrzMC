07ee128394d81bb445c4933e8b3a93c60a6abc77
wangzhizhou <824219521@qq.com>
2019-10-25 17:00:21 +0800

    script refine

--- site_publish.sh
+++ site_publish.sh
@@ -2,11 +2,11 @@
 # !/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+# if [ "`git status -s`" ]
+# then
+#     echo "The working directory is dirty. Please commit any pending changes."
+#     exit 1;
+# fi
 
 SITE_ROOT="website"
 SITE_PUB_DIR="${SITE_ROOT}/public"
@@ -29,7 +29,7 @@ echo "Generating site"
 hugo -s "${SITE_ROOT}"
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd ${SITE_PUB_DIR} && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 #echo "Pushing to github"
 #git push --all
