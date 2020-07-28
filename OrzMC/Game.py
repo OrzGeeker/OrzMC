00b8940153655d6ada540b03c9e4d6f88f54a648
wangzhizhou <wangzhizhou@bytedance.com>
2020-07-29 06:37:16 +0800

    refine OrzMC.py

--- OrzMC/Game.py
+++ OrzMC/Game.py
@@ -7,7 +7,9 @@ from .Forge import Forge
 from .OptiFine import OptiFine
 from .CleanUp import CleanUp
 from .PaperAPI import PaperAPI
-from .utils import matchAndReplace, checkFileExist, isPy3, platformType, ColorString, writeContentToFile, zip
+from .Constants import *
+from .Oracle import Oracle
+from .utils import hint, matchAndReplace, checkFileExist, isPy3, platformType, ColorString, writeContentToFile, zip
 import json
 import requests
 import os
@@ -44,6 +46,124 @@ class Game:
         self._assets=None
         self._javaClassPathList = None
         self.config = config
+
+    def userInteraction(self):
+        
+        # 显示可用版本信息
+        self.showVersionList()
+
+        # 仅客户端显示
+        self.showUserName()
+
+        # 客户端启动方案
+        self.selectLauncherProfile()
+
+        
+    def showVersionList(self):
+
+        releaseVersions = Mojang.get_release_version_id_list(update = True)
+
+        print(ALL_VERSIONS_HINT)
+
+        versionInfo = LEADING_SPACE
+        for index, releaseVersion in enumerate(releaseVersions): 
+            versionInfo = versionInfo + VERSION_FORMATTER.format(str(releaseVersion))
+            if (index + 1) % 5 == 0:
+                versionInfo = versionInfo + '\n' + LEADING_SPACE
+            else:
+                versionInfo = versionInfo + TAB_SPACE
+            if releaseVersion == '1.13': 
+                break
+
+        print(ColorString.string(versionInfo,ColorString.FG_GREEN,displayMode=ColorString.HIGHLIGHT))
+
+        if len(releaseVersions) > 0:
+            self.config.version = releaseVersions[0] # 默认版本号
+
+        select = hint(SELECT_VERSION_HINT % ('deploy' if not self.config.is_client else 'play' , DEFAULT_VERSION_HINT % self.config.version))
+        if len(select) > 0:
+            found = False
+            for releaseVersion in releaseVersions: 
+                if releaseVersion == select.strip():
+                    found = True
+                    self.config.version = releaseVersion
+                    print(CHOOSED_VERSION % self.config.version)
+            if not found:
+                print(NOT_FOUND_VERSION)
+        else:
+            print(CHOOSED_DEFAULT_VERSION % self.config.version)
+
+    def showUserName(self):
+
+        if not self.config.is_client:
+            return 
+
+        u = hint(CHOOSE_USERNAME_HINT % self.config.username)
+        if len(u) > 0:
+            self.config.username = u
+            print(CHOOSED_USERNAME % self.config.username)
+        else:
+            print(CHOOSED_DEFAULT_USERNAME)
+
+
+    def selectLauncherProfile(self):
+
+        if not self.config.is_client or not self.config.isPure or not self.config.optifine:
+            return 
+
+        launcher_profiles_json_file_path = self.config.game_version_launcher_profiles_json_path()
+
+        if os.path.exists(launcher_profiles_json_file_path):
+            content = None
+            with open(launcher_profiles_json_file_path, 'r') as f:
+                content = json.load(f)
+                profiles = content.get('profiles', None)
+                count = len(profiles)
+                if count > 0:
+                    print(ColorString.warn("There are those profiles you can choose to launch: "))
+                    index = 0
+                    selectedIndex = 0
+                    keys = profiles.keys()
+                    for key in keys:
+                        index = index + 1
+                        if key == content['selectedProfile']:
+                            selectedIndex = index
+                        isSelected = '*' if key == content['selectedProfile'] else ' '
+                        print(ColorString.hint('\t%s %s. %s' % (isSelected, index, key)))
+
+                    options = "[1 - %s] " % count if count > 1 else ""
+                    try:
+                        input = hint(ColorString.warn("Which one you choose %s: " % options))
+                        which = int(input) if input and len(input) > 0 else selectedIndex
+                        if which >= 1 and which <= count:
+                            selected_key = list(keys)[which - 1]
+                            self.config.lastVersionId = profiles.get(selected_key).get('lastVersionId')
+                            content['selectedProfile'] = selected_key
+                        else: 
+                            print(ColorString.error("There is no option you specified!"))
+                            exit(-1)
+                    except:
+                        print(ColorString.error("There is no option you specified!"))
+                        exit(-1)
+
+            if content != None: 
+                with open(launcher_profiles_json_file_path, 'w') as f: 
+                    json.dump(content, f)
+
+    def start(self):
+
+        # 检查是否安装JDK
+        Oracle.install_jdk()
+
+        # 用户交互
+        self.userInteraction()
+
+        if self.config.is_client:
+            # 启动客户端
+            self.startClient()
+        else:
+            # 启动服务端
+            self.deployServer()
 
     # 启动客户端
     def startClient(self):
