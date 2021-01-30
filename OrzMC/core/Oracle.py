53d96fe8e8a5fd1255ebc26808e34b013fe6b8a4
wangzhizhou <824219521@qq.com>
2021-01-31 04:15:48 +0800

    jdk install script failed!

--- OrzMC/core/Oracle.py
+++ OrzMC/core/Oracle.py
@@ -3,7 +3,7 @@ from ..utils.utils import platformType
 from ..utils.ColorString import ColorString
 import requests
 import os
-import progressbar
+from tqdm import tqdm
 
 def install_jdk():
     Oracle.install_jdk()
@@ -68,25 +68,12 @@ class Oracle:
         requests.packages.urllib3.disable_warnings()
         response = requests.get(Oracle.jdk_url(), stream=True, verify=False, allow_redirects=True, cookies = cookies, headers = headers)
         total_length = int(response.headers.get('content-length'))
-        widgets = [
-            'Download JDK: ',
-            progressbar.Percentage(),
-            ' ',
-            progressbar.Bar(marker='#', left='[', right=']'),
-            ' ',
-            progressbar.ETA(),
-            ' ',
-            progressbar.FileTransferSpeed()
-        ]
-        pbar = progressbar.ProgressBar(widgets=widgets, maxval=total_length).start()
-        count = 0
+        mb_chunk = 1024 * 1024
+        mb_size = int(total_length / mb_chunk + 0.5)
         with open(Oracle.jdk_temp_filename(), 'wb') as f:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-                    count += len(chunk)
-                pbar.update(count)
-            pbar.finish()
+            desc = os.path.basename(Oracle.jdk_url())
+            for chunk in tqdm(iterable=response.iter_content(mb_chunk),total=mb_size,unit='MB',desc=desc):
+                f.write(chunk)
 
     @classmethod
     def jdk_url(cls):
@@ -152,38 +139,3 @@ class Oracle:
                 ]
             }
         }
-
-    # 断点续传原理
-    # @classmethod
-    # def download(url, file_path):
-    #     # 第一次请求是为了得到文件总大小
-    #     r1 = requests.get(url, stream=True, verify=False)
-    #     total_size = int(r1.headers['Content-Length'])
-
-    #     # 这重要了，先看看本地文件下载了多少
-    #     if os.path.exists(file_path):
-    #         temp_size = os.path.getsize(file_path)  # 本地已经下载的文件大小
-    #     else:
-    #         temp_size = 0
-    #     # 显示一下下载了多少   
-    #     print(temp_size)
-    #     print(total_size)
-    #     # 核心部分，这个是请求下载时，从本地文件已经下载过的后面下载
-    #     headers = {'Range': 'bytes=%d-' % temp_size}  
-    #     # 重新请求网址，加入新的请求头的
-    #     r = requests.get(url, stream=True, verify=False, headers=headers)
-
-    #     # 下面写入文件也要注意，看到"ab"了吗？
-    #     # "ab"表示追加形式写入文件
-    #     with open(file_path, "ab") as f:
-    #         for chunk in r.iter_content(chunk_size=1024):
-    #             if chunk:
-    #                 temp_size += len(chunk)
-    #                 f.write(chunk)
-    #                 f.flush()
-
-    #                 ###这是下载实现进度显示####
-    #                 done = int(50 * temp_size / total_size)
-    #                 sys.stdout.write("\r[%s%s] %d%%" % ('█' * done, ' ' * (50 - done), 100 * temp_size / total_size))
-    #                 sys.stdout.flush()
-    #     print()  # 避免上面\r 回车符
