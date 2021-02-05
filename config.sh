d4d45312fb1082f1d4e8af02c09e848c0f6185e3
wangzhizhou <wangzhizhou@bytedance.com>
2021-02-05 17:38:23 +0800

    fix

--- config.sh
+++ config.sh
@@ -1,83 +1,71 @@
 #!/usr/bin/env bash
 #-*- utf-8 -*-
 
+function config_python_env {
+    pipenv --three  install     \
+                    install     \
+                    setuptools  \
+                    wheel       \
+                    twine       \
+                    -e .        \
+                    && pipenv shell
+}
 
-brew --version > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+function config_darwin {
 
+    brew --version > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        # https://brew.sh
+        sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        # uninstall homebrew
+        # sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+    fi
+
+    pipenv --version > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        # https://github.com/pypa/pipenv
+        brew install pipenv
+    fi
+
+    git --version > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        # https://git-scm.com/
+        brew install git
+    fi
+
+    hugo version > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        # https://gohugo.io/
+        brew install hugo
+    fi
+
+    config_python_env
+}
+
+function config_ubuntu {
+    sudo apt-get install -y \
+        git                 \
+        pipenv              \
+        hugo
+
+    config_python_env
+}
+
+function main {
     case $(uname) in
         'Linux')
-            # ubuntu 
-            sudo apt-get install linuxbrew-wrapper -y
-            # sudo apt-get remove linuxbrew-wrapper -y
-
-            # 替换brew.git
-            BASHRC=~/.bashrc
-            HOMEBREW_REPO_DIR="$(brew --repo)"
-            if [ -d "$HOMEBREW_REPO_DIR" ]; then
-                # add ENV PATH
-                HOMEBREW_BIN="export PATH=\$PATH:$(brew --repo)/../bin"
-                cat $BASHRC | grep "$HOMEBREW_BIN" > /dev/null 2>&1
-                if [ $? -ne 0 ]; then
-                    echo ""$HOMEBREW_BIN"" >> $BASHRC
-                    source $BASHRC
-                fi
-
-                cd $HOMEBREW_REPO_DIR
-                git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
-                cd -
+            LINUX_TYPE=$(echo $(cat /etc/issue) | cut -d ' ' -f 1)
+            if [ "$LINUX_TYPE" = 'Ubuntu' ]; then
+                config_ubuntu
             fi
-
-            # 替换homebrew-core.git
-            HOMEBREW_CORE_DIR="$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-            if [ -d "$HOMEBREW_CORE_DIR" ]; then
-                cd $HOMEBREW_CORE_DIR
-                git remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
-                cd -
-            fi
-
-            # 替换homebrew-bottles访问地址
-            HOMEBREW_BOTTLE='export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles'
-            cat $BASHRC | grep "$HOMEBREW_BOTTLE" > /dev/null 2>&1
-            if [ $? -ne 0 ]; then
-                echo ""$HOMEBREW_BOTTLE"" >> $BASHRC
-                source $BASHRC
-            fi
-        ;;
+            ;;
         'Darwin')
-            # https://brew.sh
-            sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-            # uninstall homebrew
-            # sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-        ;;
+            config_darwin
+            ;;
         *)
             echo 'unknown platform and os'
-        ;;
+            ;;
     esac
-fi
+}
 
-pipenv --version > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    # https://github.com/pypa/pipenv
-    brew install pipenv
-fi
-
-git --version > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    # https://git-scm.com/
-    brew install git
-fi
-
-hugo version > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    # https://gohugo.io/
-    brew install hugo
-fi
-
-pipenv --three  install     \
-                install     \
-                setuptools  \
-                wheel       \
-                twine       \
-                -e .        \
-                && pipenv shell
+main
