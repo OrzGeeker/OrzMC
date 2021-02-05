4b50181ef0ba08fb6644aa9831fcb527ff98151d
wangzhizhou <wangzhizhou@bytedance.com>
2021-02-05 15:23:40 +0800

    ubuntu install homebrew

--- config.sh
+++ config.sh
@@ -4,24 +4,33 @@
 
 brew --version > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    # https://brew.sh
-    sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    # uninstall homebrew
-    # sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 
-    if [ $(uname) == 'Linux' ]; then
-        # 替换brew.git
-        cd "$(brew --repo)"
-        git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
+    case $(uname) in
+        'Linux')
+            # ubuntu 安装homebrew
+            sudo apt-get install linuxbrew-wrapper
+            # 替换brew.git
+            cd "$(brew --repo)"
+            git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
 
-        # 替换homebrew-core.git
-        cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-        git remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
+            # 替换homebrew-core.git
+            cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+            git remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
 
-        # 替换homebrew-bottles访问地址
-        export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
-        echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.bashrc
-    fi
+            # 替换homebrew-bottles访问地址
+            export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
+            echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.bashrc
+        ;;
+        'Darwin')
+            # https://brew.sh
+            sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            # uninstall homebrew
+            # sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+        ;;
+        *)
+            echo 'unknown platform and os'
+        ;;
+    esac
 fi
 
 pipenv --version > /dev/null 2>&1
