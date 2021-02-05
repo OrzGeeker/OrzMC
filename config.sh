29b1aeea87de37cd7dde2aecf2f15c8e2fab3fa7
wangzhizhou <wangzhizhou@bytedance.com>
2021-02-05 14:37:29 +0800

    change homebrew source when install

--- config.sh
+++ config.sh
@@ -8,6 +8,20 @@ if [ $? -ne 0 ]; then
     sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     # uninstall homebrew
     # sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+
+    if [ $(uname) == 'Linux' ]; then
+        # 替换brew.git
+        cd "$(brew --repo)"
+        git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
+
+        # 替换homebrew-core.git
+        cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+        git remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
+
+        # 替换homebrew-bottles访问地址
+        export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
+        echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.bashrc
+    fi
 fi
 
 pipenv --version > /dev/null 2>&1
