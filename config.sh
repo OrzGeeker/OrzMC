a4cb28943531b115ea0a356f05faaee1acc75273
wangzhizhou <wangzhizhou@bytedance.com>
2021-02-05 13:12:30 +0800

    install pipenv

--- config.sh
+++ config.sh
@@ -3,10 +3,14 @@
 
 
 brew --version > /dev/null 2>&1
-
 if [ $? -ne 0 ]; then
     # https://brew.sh
     sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     # uninstall homebrew
     # sudo echo '^M' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+fi
+
+pipenv --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    brew install pipenv
 fi
