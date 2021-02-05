5805c570056e542319f9b01bf816b33ee957f336
wangzhizhou <wangzhizhou@bytedance.com>
2021-02-05 15:40:46 +0800

    refine config.sh

--- config.sh
+++ config.sh
@@ -7,8 +7,31 @@ if [ $? -ne 0 ]; then
 
     case $(uname) in
         'Linux')
-            # ubuntu 安装homebrew
+            # ubuntu 
             sudo apt-get install linuxbrew-wrapper
+
+            # 替换brew.git
+            HOMEBREW_REPO_DIR="$(brew --repo)"
+            if [ -d "$HOMEBREW_REPO_DIR" ]; then
+                cd $HOMEBREW_REPO_DIR
+                git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
+                cd -
+            fi
+
+            # 替换homebrew-core.git
+            HOMEBREW_CORE_DIR="$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+            if [ -d "$HOMEBREW_CORE_DIR" ]; then
+                cd $HOMEBREW_CORE_DIR
+                git remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
+            fi
+
+            # 替换homebrew-bottles访问地址
+            HOMEBREW_BOTTLE='export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles'
+            BASHRC='~/.bashrc'
+            cat $BASHRC | grep "$HOMEBREW_BOTTLE" > /dev/null 2>&1
+            if [ $? -ne 0 ]; then
+                echo ""$HOMEBREW_BOTTLE"" >> $BASHRC
+            fi
         ;;
         'Darwin')
             # https://brew.sh
