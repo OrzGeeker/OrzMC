071714a7473d629a9192efaefee884c0435d6c32
wangzhizhou <824219521@qq.com>
2021-02-06 22:19:01 +0800

    add rsync script

--- scripts/rsync/sync_server_data.sh
+++ scripts/rsync/sync_server_data.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 #-*- utf-8 -*-
+
+MC_DIR=~/minecraft_world_backup/
+SOURCE_DIR="${MC_DIR}/mcserver/"
+DEST_DIR="ubuntu@mc.jokerhub.cn:~/$(basename ${MC_DIR})/$(basename ${SOURCE_DIR}))"
+
+echo $SOURCE_DIR 
+echo $DEST_DIR
+# rsync -zarv \
+#     $SOURCE_DIR \
+#     ubuntu@mc.jokerhub.cn:~/ \
+#     --exclude 'plugins/dynmap/*' \
+#     -n
