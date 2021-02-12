c9e39b273d1d12a686d4bada96c59e05e587aa65
wangzhizhou <824219521@qq.com>
2021-02-13 00:41:35 +0800

    refix

--- OrzMC/app/Nginx.py
+++ OrzMC/app/Nginx.py
@@ -51,11 +51,11 @@ class Nginx:
         '''配置nginx服务支持https'''
 
         cmd = 'sudo apt-get update &&'\
-        'sudo apt-get install -y software-properties-common ||'\
-        'sudo add-apt-repository -y universe ||'\
-        'sudo add-apt-repository -y ppa:certbot/certbot ||'\
-        'sudo apt-get update ||'\
-        'sudo apt-get install -y certbot python3-certbot-nginx ||'\
+        'sudo apt-get install -y software-properties-common &&'\
+        'sudo add-apt-repository -y universe &&'\
+        'sudo add-apt-repository -y ppa:certbot/certbot &&'\
+        'sudo apt-get update &&'\
+        'sudo apt-get install -y certbot python3-certbot-nginx &&'\
         'sudo certbot --nginx'
         if os.system(cmd) == 0:
             print(ColorString.confirm('Config HTTPS successfully!'))
