4ba2d485929345b050cc76a5ffcaaebe07da9720
wangzhizhou <wangzhizhou@bytedance.com>
2021-02-18 12:54:51 +0800

    update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setuptools.setup(
     name="OrzMC",
-    version="1.0.91",
+    version="1.0.92",
     description="A project for deployment of minecraft game.",
     long_description=open('README.md', encoding = 'UTF-8').read(),
     long_description_content_type="text/markdown",
