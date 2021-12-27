81464cc19a773d880d45c7fb4c08e04be6ec3977
wangzhizhou <wangzhizhou@bytedance.com>
2021-12-27 19:54:16 +0800

    remove deps

--- swift/Package.swift
+++ swift/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "JokerKits", dependencies: [.product(name: "Path", package: "Path.swift")]),
+        .target(name: "JokerKits"),
         .target(
             name: "Mojang",
             dependencies: ["JokerKits"]),
