213c8f6bae1cf0ccd93035d220a15bd34ab80802
wangzhizhou <wangzhizhou@bytedance.com>
2021-12-27 19:53:38 +0800

    remove dependences

--- swift/Package.swift
+++ swift/Package.swift
@@ -11,9 +11,6 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.0"),
-        .package(url: "https://github.com/mxcl/Path.swift.git", from: "1.0.0")
-        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
