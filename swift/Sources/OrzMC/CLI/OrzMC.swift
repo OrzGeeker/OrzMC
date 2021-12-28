5483fd2c211a335242fbffb681fd869c4b68b87b
wangzhizhou <824219521@qq.com>
2021-12-29 02:50:50 +0800

    add ConsoleKit deps

--- swift/Sources/OrzMC/CLI/OrzMC.swift
+++ swift/Sources/OrzMC/CLI/OrzMC.swift
@@ -12,5 +12,18 @@ import Mojang
 @main
 struct OrzMC {
     static func main() async throws {
+        let console = Terminal()
+        let input = CommandInput(arguments: CommandLine.arguments)
+        let context = CommandContext(console: console, input: input)
+        
+        let commands = Commands(enableAutocomplete: true)
+        do {
+            let group = commands.group(help: "Group Commands")
+            try console.run(group, with: context)
+        }
+        catch let error {
+            console.error("\(error)")
+            exit(1)
+        }
     }
 }
