c62e8f3c59a3bc9a7df6d8bd3155b78d7d3135a8
wangzhizhou <824219521@qq.com>
2022-01-10 01:22:21 +0800

    change crypto deps

--- swift/Package.swift
+++ swift/Package.swift
@@ -12,12 +12,17 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vapor/console-kit.git", from: "4.0.0"),
-//        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "JokerKits"),
+        .target(
+            name: "JokerKits",
+            dependencies: [
+                .product(name: "Crypto", package: "swift-crypto"),
+            ]
+        ),
         .target(
             name: "Mojang",
             dependencies: ["JokerKits"]),
