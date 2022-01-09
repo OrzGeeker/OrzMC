0d4b01f9922c934761489057070b79326999e9dd
wangzhizhou <824219521@qq.com>
2022-01-10 01:22:21 +0800

    change crypto deps

--- swift/Sources/JokerKits/URL+Download.swift
+++ swift/Sources/JokerKits/URL+Download.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import CommonCrypto
+import CryptoKit
 
 public extension URL {
     var getData: Data? {
@@ -27,10 +27,7 @@ public extension URL {
             }
             
             let data = try Data(contentsOf: self)
-            var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
-            data.withUnsafeBytes {
-                _ = CC_SHA1($0.baseAddress, CC_LONG(data.count), &digest)
-            }
+            let digest = Insecure.SHA1.hash(data: data)
             let hexBytes = digest.map { String(format: "%02hhx", $0) }
             return hexBytes.joined()
         }
