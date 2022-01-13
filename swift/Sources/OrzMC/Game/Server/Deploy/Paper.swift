373f8c2b0f0a8510e231901f7c1198b0a904cf63
wangzhizhou <824219521@qq.com>
2022-01-14 02:30:09 +0800

    launch Paper Server Complete

--- swift/Sources/OrzMC/Game/Server/Deploy/Paper.swift
+++ swift/Sources/OrzMC/Game/Server/Deploy/Paper.swift
@@ -8,6 +8,7 @@
 import PaperMC
 import JokerKits
 import Foundation
+import ConsoleKit
 
 enum PaperServerError: Error {
     case versionRespFailed
@@ -20,9 +21,9 @@ enum PaperServerError: Error {
 struct PaperServer: Server {
     
     let deployInfo: DeployInfo
-    
+        
     func start() async throws {
-
+        
         let versionAPI = PaperMC.api.projects("paper").versions(deployInfo.version)
         guard let versionData = try await versionAPI.getData
         else {
@@ -43,7 +44,7 @@ struct PaperServer: Server {
         }
         
         let build = try decoder.decode(Build.self, from: buildData)
-       
+        
         
         guard let application = build.downloads["application"]
         else {
@@ -59,19 +60,24 @@ struct PaperServer: Server {
         let targetDir = GameDir.server(version: deployInfo.version, type: GameType.paper.rawValue)
         let filePath = targetDir.filePath(filename)
         let progressHint = "下载服务端文件：\(filename)"
-
+        
         try await GameUtils.download(
             downloadURL,
             progressHint: progressHint,
             targetDir: targetDir,
             hash: application.sha256,
             hashType: .sha256)
+                
+        try await launchServer(filePath, workDirectory: targetDir)
+    }
+    
+    func launchServer(_ filePath: String, workDirectory: GameDir) async throws {
         
         let javaPath = try Shell.run(
             path: "/usr/bin/env",
             args: ["which", "java"]).trimmingCharacters(in: .whitespacesAndNewlines)
         
-         var args = [
+        var args = [
             "-Xms512M",
             "-Xmx2G",
             "-jar",
@@ -87,22 +93,29 @@ struct PaperServer: Server {
                 print(arg)
             }
         }
-
-//        Platform.console.output("服务端正在启动请稍等...", style: .success)
-//        let process = try await Shell.run(path: javaPath, args: args, workDirectory: targetDir.dirPath)
-//        guard process.terminationStatus == 0
-//        else {
-//            print(process.terminationReason)
-//            return
-//        }
-//        Platform.console.output("服务已终止...", style: .success)
         
-        try Shell.run(path: javaPath, args: args, workDirectory: targetDir.dirPath) { process in
-            guard process.terminationStatus == 0
-            else {
-                print(process.terminationReason)
-                return
+        let eulaFilePath = workDirectory.filePath("eula.txt")
+        if let eulaFileContent = try? String(contentsOfFile: eulaFilePath) {
+            Platform.console.pushEphemeral()
+            Platform.console.warning("首次启动，未同意EULA协议")
+            try eulaFileContent.replacingOccurrences(of: "eula=false", with: "eula=true")
+                .write(toFile: eulaFilePath, atomically: false, encoding: .utf8)
+            Platform.console.popEphemeral()
+            Platform.console.success("已同意EULA协议")
+            
+            Platform.console.info("服务端正在运行中...")
+            try Shell.run(path: javaPath, args: args, workDirectory: workDirectory.dirPath) { process in
+                guard process.terminationStatus == 0
+                else {
+                    print(process.terminationReason)
+                    return
+                }
+                Platform.console.info("服务端已停止")
             }
+        }
+        else {
+            try await Shell.run(path: javaPath, args: args, workDirectory: workDirectory.dirPath)
+            try await launchServer(filePath, workDirectory: workDirectory)
         }
     }
 }
