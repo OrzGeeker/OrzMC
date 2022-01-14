9b8efdc50128e5d342f927e3a89f4fbe217fac2a
wangzhizhou <824219521@qq.com>
2022-01-14 22:12:43 +0800

    add command group

--- swift/Sources/OrzMC/CLI/OrzMC.swift
+++ swift/Sources/OrzMC/CLI/OrzMC.swift
@@ -6,17 +6,34 @@
 //
 
 import PaperMC
+import JokerKits
+import ConsoleKit
+import Darwin
 
 @main
 struct OrzMC {
     static func main() async throws {
-        // 向用户获取启动器相关的参数信息
-        if let clientInfo = try await OrzMC.userInput() {
-            try await Launcher(clientInfo: clientInfo).start()
+        
+        let console = Platform.console
+        let input = CommandInput(arguments: CommandLine.arguments)
+        let context = CommandContext(console: console, input: input)
+        
+        do {
+            let group = OrzMCCommandGroup()
+            try console.run(group, input: input)
         }
+        catch let error {
+            console.error("\(error)")
+            exit(1)
+        }
+        
+        // 向用户获取启动器相关的参数信息
+//        if let clientInfo = try await OrzMC.userInput() {
+//            try await Launcher(clientInfo: clientInfo).start()
+//        }
 //        try await VanillaServer(deployInfo: .init(version: "1.18.1")).start()
 //        try await PaperServer(deployInfo: .init(version: "1.18.1")).start()
-//        print("Hello, world!")
+        print("Hello, world!")
     }
 }
 
