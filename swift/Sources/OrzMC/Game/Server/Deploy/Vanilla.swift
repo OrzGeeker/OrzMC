272b7d2c14739d9de0b968635b2b8b5dab47a222
wangzhizhou <824219521@qq.com>
2022-01-16 18:37:17 +0800

    服务器运行为离线模式

--- swift/Sources/OrzMC/Game/Server/Deploy/Vanilla.swift
+++ swift/Sources/OrzMC/Game/Server/Deploy/Vanilla.swift
@@ -75,12 +75,24 @@ struct VanillaServer: Server {
         
         let eulaFilePath = workDirectory.filePath("eula.txt")
         if let eulaFileContent = try? String(contentsOfFile: eulaFilePath) {
+            
+            // 修改ELUA协议
             Platform.console.pushEphemeral()
             Platform.console.warning("首次启动，未同意EULA协议")
             try eulaFileContent.replacingOccurrences(of: "eula=false", with: "eula=true")
                 .write(toFile: eulaFilePath, atomically: false, encoding: .utf8)
             Platform.console.popEphemeral()
             Platform.console.success("已同意EULA协议")
+            
+            // 修改服务器属性
+            let propertiesFilePath = workDirectory.filePath("server.properties")
+            if let propertiesFileConent = try? String(contentsOfFile: propertiesFilePath) {
+                Platform.console.pushEphemeral()
+                try propertiesFileConent.replacingOccurrences(of: "online-mode=true", with: "online-mode=false")
+                    .write(toFile: propertiesFilePath, atomically: false, encoding: .utf8)
+                Platform.console.popEphemeral()
+                Platform.console.success("服务器运行为离线模式")
+            }
             
             Platform.console.info("服务端正在运行中...")
             try Shell.run(path: javaPath, args: args, workDirectory: workDirectory.dirPath) { process in
