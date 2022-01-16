f01309d30d599d8c247e6f278efbf0a3d474ab70
wangzhizhou <824219521@qq.com>
2022-01-16 17:03:36 +0800

    java install

---
+++ swift/Sources/JokerKits/JavaInstaller.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by joker on 2022/1/15.
+//
+
+import Foundation
+
+public enum JDK {
+    case jdk17
+    case jdk16
+    case jdk11
+    case jdk8
+}
+
+public struct JavaInstaller {
+    static public func install(_ jdk: JDK) {
+        
+    }
+}
